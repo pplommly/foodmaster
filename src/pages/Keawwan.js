9c3fd864b48ea53cad04e088ed15ad3a02d825e6
pplommly <33408379+pplommly@users.noreply.github.com>
2018-05-18 16:11:43 +0700

    no message

---
+++ src/pages/Keawwan.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Keawwan extends Component {
+    render() {
+        return (
+            <div>
+                <div class="bgimg-1 w3-display-container" id="home">
+          
+          </div>
+            </div>
+        );
+    }
+}
+
+export default Keawwan;
